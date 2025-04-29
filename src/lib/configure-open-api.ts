c188cd4d8bf670ad63f5fb0d55c35c0d1fd6d7f8
Paulo Diogo <pndiogo@gmail.com>
2025-04-29 23:04:39 +0100

    chore: update api docs path

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -5,23 +5,23 @@ import type { AppOpenAPI } from "./types";
 import packageJson from "../../package.json" with { type: "json" };
 
 export default function configureOpenAPI(app: AppOpenAPI) {
-  app.doc("/doc", {
+  app.doc("/api/v1/doc", {
     openapi: "3.0.0",
     info: {
       version: packageJson.version,
-      title: "Tasks API",
+      title: "Stage Locker API V1",
     },
   });
 
   app.get(
-    "/reference",
+    "/api/v1/reference",
     Scalar({
       layout: "classic",
       defaultHttpClient: {
         targetKey: "js",
         clientKey: "fetch",
       },
-      url: "/doc",
+      url: "/api/v1/doc",
     }),
   );
 }
